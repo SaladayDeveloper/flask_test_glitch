3f88e2f55b73b21c6968aefc350b90b019622077
Saladay Aleksey <saladay@yandex.ru>
2024-03-14 16:57:03 +0500

    Com1

--- data/news.py
+++ data/news.py
@@ -15,5 +15,4 @@ class News(SqlAlchemyBase, SerializerMixin):
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
-    is_published = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
